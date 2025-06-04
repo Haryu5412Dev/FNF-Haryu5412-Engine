9f853fbe1132df3e33980460460440eb3c4a9962
Haryu5412 <haryu5412@gbsw.gyo6.net>
2025-06-04 13:03:38 +0900

    0.6.1 - TEST BUILD

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -26,7 +26,7 @@ using StringTools;
 class MainMenuState extends MusicBeatState
 {
 	public static var psychEngineVersion:String = '0.6.3'; 
-	public static var harVersion:String = '0.6';
+	public static var harVersion:String = '0.6.1 - TEST BUILD';
 	public static var curSelected:Int = 0;
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
